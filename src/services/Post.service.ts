f48260ca510868a6cd91af3baeea746eaee90b63
Axel NGuessan <nguessanaxel21@gmail.com>
2025-06-26 00:55:46 +0000

    Update Post.service.ts

--- src/services/Post.service.ts
+++ src/services/Post.service.ts
@@ -45,7 +45,7 @@ export class PostService {
 
     // 1. Posts des utilisateurs suivis
     const followedPosts = await PostModel.find({ user: { $in: followedUserIds } })
-        .populate('user', 'username profilePicture')
+        .populate('user', '_id username profilePicture')
         .sort({ createdAt: -1 })
         .limit(Math.floor(limit * 0.6));
 
@@ -56,13 +56,13 @@ export class PostService {
         { $project: { _id: 1 } },
     ]);
     const randomPosts = await PostModel.find({ _id: { $in: randomPostIds.map(p => p._id) } })
-        .populate('user', 'username profilePicture');
+        .populate('user', '_id username profilePicture');
 
     // 3. Posts personnels
     const selfPosts = await PostModel.find({ user: userId })
         .sort({ createdAt: -1 })
         .limit(Math.floor(limit * 0.05))
-        .populate('user', 'username profilePicture');
+        .populate('user', '_id username profilePicture');
 
     // 4. Fusion sans doublon
     const allPostsMap = new Map<string, IPost>();
