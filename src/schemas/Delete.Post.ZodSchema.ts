e77433161ed9759eaf96ab59371a96ca7eb5aac5
Axel NGuessan <nguessanaxel21@gmail.com>
2025-09-17 15:14:26 +0000

    Update Delete.Post.ZodSchema.ts

--- src/schemas/Delete.Post.ZodSchema.ts
+++ src/schemas/Delete.Post.ZodSchema.ts
@@ -3,5 +3,4 @@ import { z } from "zod";
 
 export const DeletePostSchema = z.object({
     postId: z.string().regex(/^[0-9a-fA-F]{24}$/, "Invalid post ID format").nonempty("Post ID is required"),
-    user: z.string().regex(/^[0-9a-fA-F]{24}$/, "Invalid post ID format").nonempty("User ID is required"),
 });
