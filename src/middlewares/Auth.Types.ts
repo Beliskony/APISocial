3438f74227fb3f02e252a9b56e5b0e5a8f51f765
Axel NGuessan <nguessanaxel21@gmail.com>
2025-09-17 15:25:42 +0000

    test

--- src/middlewares/Auth.Types.ts
+++ src/middlewares/Auth.Types.ts
@@ -5,8 +5,8 @@ export interface AuthUser {
  username: string; // Username from the JWT token
  phoneNumber: string; // Optional phone number from the JWT token
  email: string; // Optional email from the JWT token
- followers?: string; // Optional followers array from the JWT token
- profilePicture?: string[]; // Optional profile picture from the JWT token
+ followers?: string[]; // Optional followers array from the JWT token
+ profilePicture?: string; // Optional profile picture from the JWT token
  posts?: string[]; // Optional posts array from the JWT token
 }
 
